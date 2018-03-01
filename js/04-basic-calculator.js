43f91cc12391bbc9853612dbf5c7b2b37fdca5bd
Alex Stolzoff <amstolzo@ucsd.edu>
2018-02-28 21:36:45 -0800

    again

--- js/04-basic-calculator.js
+++ js/04-basic-calculator.js
@@ -4,38 +4,38 @@ var x, y, z;
 
 function calculate(x, y, z) {
     "use strict";
-    x = Number(x); 
+    x = Number(x);
     y = Number(y);
     z = z.toUpperCase();
     window.console.log(z);
     var result;
-    
-//   do{ 
+
+
     switch (z) {
-    case "ADD":
-        result = x + y;
-        window.alert(x + " plus " + y + " = " + result);
-        break;
-    case "SUBTRACT":
-        result = x - y;
-        window.alert(x + " minus " + y + " = " + result);
-        break;
-    case "MULTIPLY":
-        result = x * y;
-        window.alert(x + " multiplied by " + y + " = " + result);
-        break;
-    case "DIVIDE":
-        result = x / y;
-        window.alert(x + " divided by " + y + " = " + result);
-        break;
-    default:
-        ask();
-            
+        case "ADD":
+            result = x + y;
+            window.alert(x + " plus " + y + " = " + result);
+            break;
+        case "SUBTRACT":
+            result = x - y;
+            window.alert(x + " minus " + y + " = " + result);
+            break;
+        case "MULTIPLY":
+            result = x * y;
+            window.alert(x + " multiplied by " + y + " = " + result);
+            break;
+        case "DIVIDE":
+            result = x / y;
+            window.alert(x + " divided by " + y + " = " + result);
+            break;
+        default:
+            ask();
+
     }
-//   }while(bool === true)
+
     return;
-       
-    
+
+
 }
 
 function ask() {
